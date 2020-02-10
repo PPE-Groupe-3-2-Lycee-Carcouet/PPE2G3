168f3f4c75d7085b3a16ca2fd90192df7ae6d2df
Nicolas Gabaret <nicolas.gabaret713@gmail.com>
2020-02-10 13:56:38 +0100

    Add files via upload

---
+++ PPE2G3/Vues/menu.php
@@ -0,0 +1,59 @@
+<body>
+		<div class="menu">
+	
+							<ul id="menu-bar">
+								<li><a href="#">GUIDES</a> 
+									<ul>
+										<li><a href = 'index.php?vue=guide&action=ajouter'>Ajouter un Guide</a></a></li>
+										<li><a href = 'index.php?vue=guide&action=visualiser'>Voir les Guides</a></li>
+										<li><a href="construction.php"><font color=#908E8E>Modifier un guide</font></a></li>
+									</ul>
+								</li>
+								<li><a href="#">PAYS</a>
+									<ul>
+										<li><A href = 'index.php?vue=pays&action=ajouter'>Ajouter un Pays</A></li>
+										<li><A href = 'index.php?vue=pays&action=visualiser'>Voir les Pays</A></li>
+										<li><a href="construction.php"><font color=#908E8E>Modifier un Pays</font></a></li>
+									</ul>
+								</li>
+
+								<li><a href="#">LANGUES</a>
+									<ul>
+										<li><a href='index.php?vue=langue&action=ajouter'>Ajouter une langue</a></li>
+										<li><a href='index.php?vue=langue&action=visualiser'>Voir une Langue</a></li>
+										<li><a href="construction.php"><font color=#908E8E>Modifier une Langue</font></a></li>
+									</ul>
+								</li>
+
+							    <li><a href="#">VOYAGES</a>
+									<ul>
+										<li><a href="construction.php"><font color=#908E8E>Ajouter un Voyage</font></a></li>
+										<li><a href="construction.php"><font color=#908E8E>Voir un Voyage</font></a></li>
+										<li><a href="construction.php"><font color=#908E8E>Modifier un Voyage</font></a></li>
+										<li><a href="construction.php"><font color=#908E8E>Voir les clients d'un Voyage</font></a></li>
+									</ul>
+							    </li>
+							</ul>
+		</div>
+
+		<div class="slider">
+					<p class="slides">	
+						<div class=slide> <img  src="Images/test3.jpg"></img></slide></div>
+						<div class=slide> <img  src="Images/diapo3.jpg"></img></slide></div>
+						<div class=slide> <img  src="Images/fond2.jpeg"></img></slide></div>
+					</p>			
+		</div>
+
+					
+				<div class="slideshow">
+					<p class="sContent">	
+						<img src="Images/imag1"></img></slide>
+						<img src="Images/imag2"></img></slide>
+						<img src="Images/imag3"></img></slide>
+					</p>
+					
+				</div>
+
+				
+</body>
+			
